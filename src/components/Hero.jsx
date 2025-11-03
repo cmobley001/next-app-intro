96bf70e750abcfb8765edccc7e62d1c71cf8d31c
cmobley001 <cmobley001@student.butte.edu>
2025-11-03 06:09:07 -0800

    minor updates to copy

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,11 @@ export default function Hero() {
     <>
       <div className="mb-6 min-h-[300px]">
         <h1 className="text-2xl font-bold text-center mt-6 gap-y-2 p-6">My First React Page</h1>
-        <LabWeek week="10" title="Learning React Children">
+        <LabWeek week="10" title="Next.js Introduction">
           <p className="mt-2">
             Github repo:{' '}
-            <a href="https://github.com/cmobley001/react-introduction" target="_blank">
-              https://github.com/cmobley001/react-introduction
+            <a href="https://github.com/cmobley001/next-app-intro" target="_blank">
+              https://github.com/cmobley001/next-app-intro
             </a>
           </p>
         </LabWeek>
