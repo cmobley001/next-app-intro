cb7d1867dd8eeacbb05fac1ec9cae2dd3db88f83
cmobley001 <cmobley001@student.butte.edu>
2025-11-03 05:48:41 -0800

    minor code fixes and style changes

--- src/components/LabWeek.jsx
+++ src/components/LabWeek.jsx
@@ -2,7 +2,7 @@ export default function LabWeek({ week, title, children }) {
   return (
     <div className="text-center">
       <h2 className="text-lg">
-        This is Week #{week} for Lab "{title}"
+        This is Week <span className="font-bold">#{week}</span> for Lab <span className="font-bold">{title}</span>
       </h2>
       {children}
     </div>
