9510e7e3e84b0733312bb40233e7c405b3132f2d
cmobley001 <cmobley001@student.butte.edu>
2025-11-08 10:11:42 -0800

    added vercel analytics and page speed insights

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,16 +1,16 @@
-import "../index.css";
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import '../index.css'
 
 export const metadata = {
-  title: "React & Next.js Introduction",
-  description: "Next.js App project",
-};
+  title: 'React & Next.js Introduction',
+  description: 'Next.js App project',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
